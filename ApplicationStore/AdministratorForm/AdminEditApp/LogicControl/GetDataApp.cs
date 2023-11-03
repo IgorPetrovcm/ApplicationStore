2f6909a8728ffaed9403a003684638740144ace8
Honor <igorpetrov42ru@gmail.com>
2023-11-03 13:17:12 +0300

    Была ошибка вывода приложений, как правило если твоя роль выше роли доступной приложению, то у тебя тоже отображается оно, но у админа отображались тольько приложения доступные айди его роли в информации о приложению,. Добавил пока что костыли, оно работает, но позже надо многое переписать

--- ApplicationStore/AdministratorForm/AdminEditApp/LogicControl/GetDataApp.cs
+++ ApplicationStore/AdministratorForm/AdminEditApp/LogicControl/GetDataApp.cs
@@ -15,9 +15,10 @@ namespace ApplicationStore_AdministratorForm_Edit
     {
         public static List<App> GetAppTest(User user)
         {
+            MySqlDataReader reader;
 
-            MySqlDataReader reader = GetResultDB.GetReader($"select * from application_test where app_role_id = {user.IdRole}");
-
+            reader = GetResultDB.GetReader($"select * from application_test where app_role_id = 1");
+            
             List<App> apps = new List<App>();
 
             while (reader.Read())
@@ -34,6 +35,26 @@ namespace ApplicationStore_AdministratorForm_Edit
 
                 apps.Add(app);
             }
+            if (user.IdRole == 2)
+            {
+                reader = GetResultDB.GetReader($"select * from application_test where app_role_id = {2}");
+                while (reader.Read())
+                {
+                    App app = new App();
+
+                    app.Id = (byte)reader.GetValue(0);
+                    app.Image = (byte[])reader.GetValue(1);
+                    app.Name = (string)reader.GetValue(2);
+                    app.Description = (string)reader.GetValue(3);
+                    app.RoleId = (byte)reader.GetValue(4);
+                    app.UserId = (byte)reader.GetValue(5);
+                    app.Restrictions = (bool)reader.GetValue(6);
+
+                    apps.Add(app);
+                }
+
+            }
+
             return apps;
 
         }
